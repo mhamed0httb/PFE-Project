b4a93c08a0fe04e5e2260fe9d8da895b2ce81c19
mhamed.httb <mhamed.httb@gmail.com>
2015-03-11 04:27:54 +0100

    form contact 01

--- src/Mhamed/CeciliaBundle/Controller/MainController.php
+++ src/Mhamed/CeciliaBundle/Controller/MainController.php
@@ -10,6 +10,8 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use OC\UserBundle\Entity\User;
+use Mhamed\CeciliaBundle\Entity\Contact;
 
 class MainController extends Controller
 {
@@ -26,7 +28,13 @@ class MainController extends Controller
 
 
 	public function testAction(){
-		return $this->render('MhamedCeciliaBundle:Main:test sante page.html.twig');
+		$user=$this->getUser();
+		if(null===$user){
+			return $this->render('MhamedCeciliaBundle:Main:test sante page.html.twig');
+		}else{
+			return $this->render('MhamedCeciliaBundle:Main:test sante user page.html.twig');
+		}
+		
 	}
 
 
@@ -78,6 +86,58 @@ class MainController extends Controller
 		$adv4 = $rep->find(6);
 		$adv=array($adv1,$adv2,$adv3,$adv4);
 		return $this->render('MhamedCeciliaBundle:Main:conseils page.html.twig',array('theAdvise'=>$adv));
+
+	}
+
+
+	public function QaAction(Request $request){
+		// On crée un objet Advert
+    $con = new Contact();
+
+    // On crée le FormBuilder grâce au service form factory
+    $formBuilder = $this->get('form.factory')->createBuilder('form', $con);
+
+    // On ajoute les champs de l'entité que l'on veut à notre formulaire
+    $formBuilder
+      ->add('name',      'text',array('attr' => array('class'=>'form-control','placeholder'=>'Your Name *','id'=>'name')))
+      ->add('phone',   'number',array('attr' => array('class'=>'form-control','placeholder'=>'Your Phone *','id'=>'phone','data-validation-required-message'=>'Please enter your phone number.')))
+      ->add('email',    'email',array('attr' => array('class'=>'form-control','placeholder'=>'Your Email *','id'=>'email')))
+      ->add('message',    'textarea',array('attr' => array('class'=>'form-control','placeholder'=>'Your Message *','id'=>'message')))
+      ->add('Send',      'submit',array('attr' => array('class'=>'btn btn-xl')))
+    ;
+    // Pour l'instant, pas de candidatures, catégories, etc., on les gérera plus tard
+
+    // À partir du formBuilder, on génère le formulaire
+    $form = $formBuilder->getForm();
+
+
+
+    $form->handleRequest($request);
+
+    // On vérifie que les valeurs entrées sont correctes
+    // (Nous verrons la validation des objets en détail dans le prochain chapitre)
+    if ($form->isValid()) {
+
+      $em = $this->getDoctrine()->getManager();
+      $em->persist($con);
+      $em->flush();
+
+      $url=$this->get('router')->generate('mhamed_cecilia_homepage');
+      return new RedirectResponse($url);
+
+
+    }
+
+    // À ce stade, le formulaire n'est pas valide car :
+    // - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
+    // - Soit la requête est de type POST, mais le formulaire contient des valeurs invalides, donc on l'affiche de nouveau
+    return $this->render('MhamedCeciliaBundle:Main:adduser.html.twig', array(
+      'form' => $form->createView(),
+    ));
+
+	}
+	public function modalAction(){
+		return $this->render('MhamedCeciliaBundle:Main:modal.html.twig');
 
 	}
 
